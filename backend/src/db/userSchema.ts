e67f0b2aba4c6089e58bd6404adb94404c2d2521
faridulakthar <faridulakthar@gmail.com>
2024-12-11 13:47:14 +0530

    feat: users table created in the drizzle orm

--- backend/src/db/userSchema.ts
+++
@@ -1,8 +0,0 @@
-// import { varchar } from "drizzle-orm/mysql-core";
-// import { integer, pgTable } from "drizzle-orm/pg-core";
-
-// export const userTable = pgTable("user", {
-//   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-//   email: varchar({ length: 255 }).notNull(),
-//   password: varchar({ length: 255 }).notNull(),
-// });
